3d9f903f650f7935717f82579d24228d1abdd0bf
Ajith Kumar <155445498+Ajith0230@users.noreply.github.com>
2024-04-04 22:19:56 +0530

    Add files via upload

---
+++ src/main/java/controller/DeleteTask.java
@@ -0,0 +1,47 @@
+package controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.Dao;
+import dto.Task;
+import dto.User;
+
+@WebServlet("/deletetask")
+public class DeleteTask extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		
+		int taskid = Integer.parseInt(req.getParameter("taskid"));
+		Dao dao = new Dao();
+		try {
+				dao.deleting(taskid);
+			
+				HttpSession session = req.getSession();
+				User user = (User)session.getAttribute("user");
+				
+				req.setAttribute("tasks", dao.getAllTasks(taskid));
+				
+				req.getRequestDispatcher("home.jsp").include(req, resp);
+		
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ src/main/java/controller/EditTask.java
@@ -0,0 +1,45 @@
+package controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.Dao;
+import dto.Task;
+import dto.User;
+
+@WebServlet("/edittask")
+public class EditTask extends HttpServlet{
+
+	
+		@Override
+		protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+			
+			HttpSession session = req.getSession();
+			Task taskvalue = (Task) session.getAttribute("taskvalue");
+			
+			Dao dao = new Dao();
+			
+			try {
+				dao.editTask(taskvalue);
+				
+				
+				User user = (User)session.getAttribute("user");
+				
+				req.setAttribute("tasks", dao.getAllTasks(user.getUserid()));
+				
+				req.getRequestDispatcher("home.jsp").include(req, resp);
+				
+				
+			} catch (ClassNotFoundException | SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+}
